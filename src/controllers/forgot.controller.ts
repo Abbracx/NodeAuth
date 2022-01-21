aa7f14f2be752c764ff6644b4be9085138abba4f
Abbracx <tankoraphael@gmail.com>
2022-01-21 12:49:25 +0100

    Commit to be publish on github

--- src/controllers/forgot.controller.ts
+++ src/controllers/forgot.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { ResetModel } from '../models/reset.model';
 import { createTransport } from 'nodemailer';
+import { UserModel } from '../models/user.model';
+import bcryptjs from 'bcryptjs'
 
 const transporter = createTransport({
     host: '0.0.0.0',
@@ -22,5 +24,29 @@ export const forgot = async ( req: Request, res: Response ) => {
         subject: 'Reset your password',
         html: `Click <a href="${url}">here</a> to reset your password`
     })
-    res.send({ message: 'success' })
+    res.send({ message: 'check your email. ' })
+}
+
+export const reset = async(req: Request, res:Response) => {
+    const body = req.body
+
+    if(body.password !== body.password_confirm){
+        return res.status(400).send({ message: 'password do not match.' })
+    }
+
+    const resetPassword = await ResetModel.findOne({ token: body.token })
+    if(!resetPassword){
+        return res.status(400).send({ message: 'Invalid link!' })
+    }
+
+    const { email } = resetPassword.toJSON()
+    const user = await UserModel.findOne({ email })
+    if(!user){
+        return res.status(404).send({ message: 'User does not exist.' })  
+    }
+
+    const salt = await bcryptjs.genSalt(10)
+    user.password = await bcryptjs.hash(body.password, salt)
+    await user.save()
+    res.send('success')
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { register, login, user, logout} from './controllers/auth.controller'
-import { forgot } from './controllers/forgot.controller';
+import { forgot, reset } from './controllers/forgot.controller';
 
 export const routes = ( router: Router) => {
     router.post("/api/register", register),
     router.post("/api/login", login);
     router.get("/api/user", user);
     router.post("/api/logout", logout);
-    router.post("/api/forgot", forgot)
+    router.post("/api/forgot", forgot);
+    router.post("/api/reset", reset);
 }  
